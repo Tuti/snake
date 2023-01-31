6b581b3b9956116397e46a2bf1155b99b912b0e3
Tuti <josehuerta115@gmail.com>
2023-01-31 13:56:32 -0800

    working animation with user input

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+const SNAKE_COLOR = '#50C878';
+const VELOCITY = 2;
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
 const tileAtlas = new Image();
 tileAtlas.src = './sprites/map.png';
 
 const map = {
   cols: 8,
   rows: 8,
-  tsize: 64,
+  tSize: 64,
   tiles: [
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -15,45 +21,105 @@ const map = {
   },
 };
 
-function draw() {
-  const canvas = document.getElementById('canvas');
-  const context = canvas.getContext('2d');
-
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.drawImage(tileAtlas, 100, 100);
-}
+const snake = {
+  x: 0,
+  y: 0,
+  tSize: 64,
+  vx: 1, //determines direction | pos == right | neg == left
+  vy: 1, //determines direction | pos == down  | neg == up
+  xCurrentDirection: true,
+  directionChanged: false,
+  draw() {
+    ctx.strokeStyle = SNAKE_COLOR;
+    ctx.fillRect(this.x, this.y, this.tSize, this.tSize);
+  },
+};
 
 function drawMap() {
-  const canvas = document.getElementById('canvas');
-  const context = canvas.getContext('2d');
-
   for (let c = 0; c < map.cols; c++) {
     for (let r = 0; r < map.rows; r++) {
       const tile = map.getTile(c, r);
       if (tile !== 0) {
         // 0 => empty tile
-        context.drawImage(
+        ctx.drawImage(
           tileAtlas, // image
-          (tile - 1) * map.tsize, // source x
+          (tile - 1) * map.tSize, // source x
           0, // source y
-          map.tsize, // source width
-          map.tsize, // source height
-          c * map.tsize, // target x
-          r * map.tsize, // target y
-          map.tsize, // target width
-          map.tsize // target height
+          map.tSize, // source width
+          map.tSize, // source height
+          c * map.tSize, // target x
+          r * map.tSize, // target y
+          map.tSize, // target width
+          map.tSize // target height
         );
 
-        context.strokeRect(c * map.tsize, r * map.tsize, map.tsize, map.tsize);
+        ctx.strokeStyle = 'black';
+        ctx.strokeRect(c * map.tSize, r * map.tSize, map.tSize, map.tSize);
       }
     }
   }
 }
 
+function drawSnake() {
+  snake.draw();
+  updateDirection();
+  if (snake.xCurrentDirection && snake.y % 64 === 0) {
+    snake.x += snake.vx * VELOCITY;
+  }
+
+  if (!snake.xCurrentDirection && snake.x % 64 === 0) {
+    snake.y += snake.vy * VELOCITY;
+  }
+}
+
+function updateDirection() {
+  if (snake.directionChanged && snake.xCurrentDirection && snake.x % 64 === 0) {
+    console.log('updated direction to y');
+    snake.xCurrentDirection = false;
+    snake.directionChanged = false;
+  }
+  if (
+    snake.directionChanged &&
+    !snake.xCurrentDirection &&
+    snake.y % 64 === 0
+  ) {
+    console.log('updated direction to x');
+    snake.xCurrentDirection = true;
+    snake.directionChanged = false;
+  }
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.width);
+  drawMap();
+  drawSnake();
+}
+
+function handleKeyboardInput() {
+  document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    if (keyName === 'ArrowUp') {
+      snake.directionChanged = true;
+      snake.vy = -1;
+    } else if (keyName === 'ArrowDown') {
+      snake.directionChanged = true;
+      snake.vy = 1;
+    } else if (keyName === 'ArrowLeft') {
+      snake.directionChanged = true;
+      snake.vx = -1;
+    } else if (keyName === 'ArrowRight') {
+      snake.directionChanged = true;
+      snake.vx = 1;
+    }
+  });
+}
+
 (() => {
   function main() {
     window.requestAnimationFrame(main);
-    drawMap();
+    draw();
   }
+
+  handleKeyboardInput();
   main();
 })();
